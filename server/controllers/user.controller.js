8331f34eada207593088bce2160f7e3a1093526d
danizavtz <daniellucena@yahoo.com.br>
2020-05-18 02:34:13 -0300

    Inserção de operacoes de get no usuario

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,30 @@
 const tipo = 'Pessoa'
 exports.listUsers = async(req, res) => {
-    const dbi = req.app.get('datastore')
-    const queryusers = dbi.createQuery(tipo);
-    const usuarios = await dbi.runQuery(queryusers);
-    res.status(200).json(usuarios);
+    try {
+        const dbi = req.app.get('datastore')
+        const queryusers = dbi.createQuery(tipo);
+        const usuarios = await dbi.runQuery(queryusers);
+        res.status(200).json(usuarios);
+    } catch (e) {
+        res.status(500).json({errors: [{location: req.path, msg: e.message, param: null}]})
+    }
+}
+
+exports.findUserById = async(req, res, next) => {
+    try {
+        const dbi = req.app.get('datastore')
+        const chave = dbi.key([tipo, Number(req.params.id)]);
+        const usuario = await dbi.get(chave)
+        if (usuario[0] === undefined) {
+            return res.status(404).json({errors: [{location: req.path, msg: "Not found", param: req.params.id}]})
+        }
+        req.usuario = usuario
+        next()
+    } catch (e) {
+        res.status(500).json({errors: [{location: req.path, msg: e.message, param: null}]})
+    }
+}
+
+exports.resultado = (req, res) => {
+    res.status(200).json(req.usuario);
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/users', userController.listUsers);
+router.get('/users/:id', userController.findUserById, userController.resultado);
 
 module.exports = router;

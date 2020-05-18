18edbc7b020b6f501421f32e0f2043df808a822a
danizavtz <daniellucena@yahoo.com.br>
2020-05-18 03:18:28 -0300

    Inserção de post, put delete

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,30 +1,76 @@
 const tipo = 'Pessoa'
-exports.listUsers = async(req, res) => {
+exports.listUsers = async (req, res) => {
     try {
         const dbi = req.app.get('datastore')
         const queryusers = dbi.createQuery(tipo);
         const usuarios = await dbi.runQuery(queryusers);
         res.status(200).json(usuarios);
     } catch (e) {
-        res.status(500).json({errors: [{location: req.path, msg: e.message, param: null}]})
+        res.status(500).json({ errors: [{ location: req.path, msg: e.message, param: null }] })
     }
 }
 
-exports.findUserById = async(req, res, next) => {
+exports.findUserById = async (req, res, next) => {
     try {
         const dbi = req.app.get('datastore')
         const chave = dbi.key([tipo, Number(req.params.id)]);
         const usuario = await dbi.get(chave)
         if (usuario[0] === undefined) {
-            return res.status(404).json({errors: [{location: req.path, msg: "Not found", param: req.params.id}]})
+            return res.status(404).json({ errors: [{ location: req.path, msg: "Not found", param: req.params.id }] })
         }
         req.usuario = usuario
         next()
     } catch (e) {
-        res.status(500).json({errors: [{location: req.path, msg: e.message, param: null}]})
+        res.status(500).json({ errors: [{ location: req.path, msg: e.message, param: null }] })
     }
 }
 
 exports.resultado = (req, res) => {
     res.status(200).json(req.usuario);
+}
+
+exports.criarUsuario = async (req, res) => {
+    try {
+        const dbi = req.app.get('datastore')
+        const chaveusuario = dbi.key(tipo)
+        const entidadeUsuario = {
+            key: chaveusuario,
+            data: [
+                req.body
+            ]
+        }
+        await dbi.save(entidadeUsuario)
+        res.set('location', `/users/${chaveusuario.id}`)
+        res.status(201).end()
+    } catch (e) {
+        res.status(500).json({ errors: [{ location: req.path, msg: e.message, param: null }] })
+    }
+}
+
+exports.atualizar = async (req, res) => {
+    const dbi = req.app.get('datastore')
+    const transaction = dbi.transaction();
+    try {
+        const dbi = req.app.get('datastore')
+        const chave = dbi.key([tipo, req.params.id])
+        const transaction = dbi.transaction();
+        const [usuario] = await transaction.get(chave);
+        transaction.save({ key: chave, data: req.body });
+        await transaction.commit();
+        res.status(200).json(usuario)
+    } catch (e) {
+        transaction.rollback();
+        res.status(500).json({ errors: [{ location: req.path, msg: e.message, param: null }] })
+    }
+}
+
+exports.apagar = async (req, res) => {
+    try {
+        const dbi = req.app.get('datastore')
+        const chave = dbi.key([tipo, req.params.id])
+        await dbi.delete(chave)
+        res.status(204).end()
+    } catch (e) {
+        res.status(500).json({ errors: [{ location: req.path, msg: e.message, param: null }] })
+    }
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
+router.post('/users', userController.criarUsuario);
 router.get('/users', userController.listUsers);
 router.get('/users/:id', userController.findUserById, userController.resultado);
+router.put('/users/:id', userController.findUserById, userController.atualizar);
+router.delete('/users/:id'. userController.findUserById, userController.apagar);
 
 module.exports = router;
